d48c7bf49b42659fa983e2e572142cd8d14c1897
victor@victor <victor.dmaina@gmail.com>
2024-05-18 16:46:19 +0300

    rewrite functions in python

--- lib/functions.py
+++ lib/functions.py
@@ -14,3 +14,18 @@ def add(num1, num2):
 
 def halve(number):
     pass
+
+def greet_programmer():
+    print("Hello, programmer!")
+
+def greet(name = "Victor"):
+    print(f"Hello, {name}!")
+
+def greet_with_default(name = "programmer"):
+    print(f"Hello, {name}!")
+
+def add(num1 = 45, num2 = 55):
+    return num1 + num2
+
+def halve(num = 4):
+    return num / 2
